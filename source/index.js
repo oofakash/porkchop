e8d7a19bbf2f66def3e1022d6d83ed59132ab8fd
Akash <akash_sharma4851@hotmail.com>
2023-07-24 21:08:59 +0530

    added db connection

--- source/index.js
+++ source/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js')
-const eventHandler = require('./handlers/eventHandler')
+const eventHandler = require('./handlers/eventHandler');
+const mongoose = require('mongoose');
 
 const client = new Client({
     intents: [
@@ -11,6 +12,26 @@ const client = new Client({
     ]
 })
 
-eventHandler(client)
+const db = process.env.DB_URI;
 
-client.login(process.env.TOKEN);
+(async () => {
+
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(db, {
+            // keepAlive: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useFindAndModify: false
+        });
+        console.log('Connected to database.');
+
+        eventHandler(client);
+
+        client.login(process.env.TOKEN);
+    } catch (error) {
+        console.log(error)
+    }
+
+
+})();
